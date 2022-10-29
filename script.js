9a652ec3fb3e7cadead94492bee0dbc2974057b4
sohodoll <kovalevvtl@gmail.com>
2022-10-29 17:40:19 +0300

    feat: add universal functions to js and create grid layouts
    Add functions to create divs, remove all child nodes, populate
    the grid, and draw the grid with user input. User can enter the
    size and press start to draw grid layout.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const grid = document.querySelector('.grid__wrapper');
-
+const sizeInput = document.querySelector('#size');
+const sizeLabel = document.querySelector('.size__label');
+const startButton = document.querySelector('.start');
 
 //function creates required number of elements and returns an array of
 //these new elements
@@ -21,9 +23,40 @@ const populateElement = (parent, child) => {
     parent.appendChild(child);
 }
 
+//change grid column template according to the number of squares
 
-let divs = createElement('div', 'grid__element', 4);
+const sizeGrid = () => {
+    grid.style.setProperty('grid-template-rows', `repeat(${size}, 1fr)`);
+    grid.style.setProperty('grid-template-columns', `repeat(${size}, 1fr)`);
+}
 
-divs.forEach(div => {
-    populateElement(grid, div);
+//get size from the user
+
+let size = 1;
+
+sizeInput.addEventListener('input', () => {
+    sizeLabel.textContent = `${sizeInput.value}x${sizeInput.value}`;
+    size = sizeInput.value;
 })
+
+//remove all child elements from the parent (clear the grid)
+
+const removeAllChildNodes = (parent) => {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+//draw grid according to the user input
+
+const drawGrid = () => {
+    removeAllChildNodes(grid);
+    let numberOfElements = size**2;
+    squaresArray = createElement('div', 'grid__element', numberOfElements);
+    squaresArray.forEach(square => {
+        populateElement(grid, square);
+    })
+    sizeGrid();
+};
+
+startButton.addEventListener('click', drawGrid);
